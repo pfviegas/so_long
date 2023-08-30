45930ff248f39e064fed00021a144434d2fb7123
paulo <paulo@Ububtu.myguest.virtualbox.org>
2023-08-30 18:26:37 +0100

    v1.15_bonus

---
+++ src_bonus/checks_bonus.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checks_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paulo <paulo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 14:19:02 by pviegas           #+#    #+#             */
+/*   Updated: 2023/08/30 18:07:08 by paulo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/so_long_bonus.h"
+
+// verifica se o programa foi chamado com o número correto de argumentos 
+// e se o argumento fornecido tem a extensão .ber. 
+void	check_args(t_game *game, int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		quit("Invalid number of argument.", game, 1);
+	}
+	if (ft_strncmp(argv[1] + ft_strlen(argv[1]) - 4, ".ber", 4))
+	{
+		quit("File must be of type .ber", game, 2);
+	}
+}
+
+// verifica se o mapa do jogo cumpre com as condições necessárias.
+void	check_map(t_game *game)
+{
+	int	line;
+	int	col;
+
+	line = 0;
+	while (game->map[line] != NULL)
+	{
+		col = -1;
+		while (game->map[line][++col])
+			check_char(game, game->map[line][col], line, col);
+		line++;
+	}
+	if (line == col)
+		quit("The map must be rectangular.", game, 6);
+	if (game->collectibles == 0)
+		quit("There's no collectibles.", game, 7);
+	else if (game->exit == 0)
+		quit("There's no exit.", game, 8);
+	else if (game->exit > 1)
+		quit("Just one exit per map.", game, 9);
+	else if (game->player == 0)
+		quit("There's no player.", game, 10);
+	else if (game->player > 1)
+		quit("Just one player per map.", game, 11);
+}
+
+// soma os collectibles e verifica se os caracteres sao validos 
+void	check_char(t_game *game, char c, int line, int col)
+{
+	if (c == 'C')
+		game->collectibles++;
+	else if (c == 'E')
+		game->exit++;
+	else if (c == 'D')
+		game->danger++;
+	else if (c == 'P')
+	{
+		game->player++;
+		game->player_x = col;
+		game->player_y = line;
+	}
+	else if (c == '1' || c == '0')
+		return ;
+	else
+		quit("Invalid characters.", game, 5);
+}
+
+// verifica se o mapa está cercado por paredes.
+void	check_walls(t_game *game)
+{
+	int	line;
+	int	col;
+
+	line = 0;
+	while (game->map[line])
+	{
+		col = 0;
+		while (game->map[line][col])
+		{
+			if (game->map[0][col] != '1' ||
+				game->map[game->line - 1][col] != '1')
+				quit("The map is not surrounded by walls", game, 12);
+			if (game->map[line][0] != '1' ||
+				game->map[line][game->column - 1] != '1')
+				quit("The map is not surrounded by walls", game, 12);
+			col++;
+		}
+		line++;
+	}
+}
+
+// verifica se o caminho do jogador é válido.
+void	check_path(t_game *game)
+{
+	if (!floodfill(game))
+	{
+		quit("Invalid path on the map", game, 13);
+	}
+	free_map_floodfill(game);
+}
---
+++ src_bonus/so_long_bonus.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long_bonus.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paulo <paulo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 14:11:04 by pviegas           #+#    #+#             */
+/*   Updated: 2023/08/30 18:11:14 by paulo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/so_long_bonus.h"
+
+// inicializa as variáveis do jogo
+static void	init_var(t_game *game)
+{
+	game->map = NULL;
+	game->map_floodfill = NULL;
+	game->collectibles = 0;
+	game->player_x = 0;
+	game->player_y = 0;
+	game->player_on_exit = 0;
+	game->exit = 0;
+	game->player = 0;
+	game->line = 0;
+	game->column = 0;
+	game->end_game = 0;
+	game->move = 1;
+	game->img.wall = NULL;
+	game->img.player = NULL;
+	game->img.floor = NULL;
+	game->img.exit = NULL;
+	game->img.collectible = NULL;
+	game->img.on_exit = NULL;
+	game->img.danger = NULL;
+}
+
+// valida o mapa, as paredes e o caminho
+static void	validations(t_game *game)
+{
+	check_map(game);
+	if (game->column <= 0)
+		quit("Invalid number of columns.", game, 14);
+	if (game->line <= 0)
+		quit("Invalid number of lines.", game, 15);
+	check_walls(game);
+	check_path(game);
+}
+
+// inicializa o ambiente gráfico, as imagens e o jogo
+void	start_game(t_game *game)
+{
+	game->mlx = mlx_init();
+	game->win = mlx_new_window(game->mlx, game->column * 64,
+			game->line * 64, "So_long");
+	init_images(game);
+	render_map(game);
+	mlx_hook(game->win, 02, 1L << 0, key_handling, game);
+	mlx_hook(game->win, 17, 1L << 17, exit_game, game);
+	mlx_loop(game->mlx);
+}
+
+int	main(int argc, char **argv)
+{
+	t_game	game;	
+	int		fd_map;
+
+	init_var(&game);
+	check_args(&game, argc, argv);
+	fd_map = open(argv[1], O_RDONLY);
+	game.line = get_lines(&game, fd_map);
+	close(fd_map);
+	fd_map = open(argv[1], O_RDONLY);
+	get_map(&game, fd_map);
+	close(fd_map);
+	validations(&game);
+	start_game(&game);
+	return (0);
+}
